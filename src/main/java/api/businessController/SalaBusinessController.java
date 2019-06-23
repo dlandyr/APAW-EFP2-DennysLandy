eadd1808125cd3ad806ce287b2e0699a435a7ea8
Dennys <dennys.landy@hotmail.com>
2019-06-23 05:20:33 +0200

    add method modificarDescripcionSala #4

--- src/main/java/api/businessController/SalaBusinessController.java
+++ src/main/java/api/businessController/SalaBusinessController.java
@@ -3,6 +3,7 @@ package api.businessController;
 import api.daos.DaoFactory;
 import api.dtos.SalaDto;
 import api.entities.Sala;
+import api.exceptions.NotFoundException;
 
 public class SalaBusinessController {
 
@@ -10,5 +11,11 @@ public class SalaBusinessController {
         Sala sala = new Sala(salaDto.getId(),salaDto.getDescripcion(),salaDto.getNumeroAsientos());
         DaoFactory.getFactory().getSalaDao().save(sala);
         return sala.getId();
+    }
+
+    public void modificarDescripcionSala(String id, SalaDto salaDto){
+        Sala sala = DaoFactory.getFactory().getSalaDao().read(id).orElseThrow(() -> new NotFoundException("Sala id: " + id));
+        sala.setDescripcion(salaDto.getDescripcion());
+        DaoFactory.getFactory().getSalaDao().save(sala);
     }
 }
