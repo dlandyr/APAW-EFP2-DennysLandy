4856b5b7ac1d2aec1de3e950ad4709c6fa2b8d74
Dennys <dennys.landy@hotmail.com>
2019-06-23 04:54:54 +0200

    refactoring #3

--- src/test/java/api/ProyeccionIT.java
+++ src/test/java/api/ProyeccionIT.java
@@ -1,11 +1,8 @@
 package api;
 
-import api.apiControllers.PeliculaApiController;
 import api.apiControllers.ProyeccionApiController;
-import api.apiControllers.SalaApiController;
 import api.daos.DaoFactory;
 import api.daos.memory.DaoMemoryFactory;
-import api.dtos.PeliculaDto;
 import api.entities.Sala;
 import api.entities.Pelicula;
 import api.dtos.ProyeccionDto;
@@ -33,7 +30,7 @@ public class ProyeccionIT {
         Sala sala= new Sala("001","sala01",45);
         Pelicula pelicula=new Pelicula("500","avatar","",2018,Genero.COMEDIA);
         LocalDateTime fecha= LocalDateTime.now();
-        this.crear("500",fecha,"19h00",sala,pelicula);
+        this.crear("500",fecha,"19h00,18h00,21h00",sala,pelicula);
     }
     private String crear(String id, LocalDateTime fecha, String hora, Sala sala, Pelicula pelicula){
         HttpRequest request = HttpRequest.builder(ProyeccionApiController.PROYECCIONES).body(new ProyeccionDto(id, fecha, hora, sala, pelicula)).post();
@@ -53,6 +50,5 @@ public class ProyeccionIT {
         HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
         assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
     }
-
 
 }
