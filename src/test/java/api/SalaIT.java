9c9d981a9b99dd02a2e79c52ae93bfd7c9f75e06
Dennis <dennys.landy@hotmail.com>
2019-06-25 18:56:09 +0200

    add method testLeerTodo() SalaIT #10

--- src/test/java/api/SalaIT.java
+++ src/test/java/api/SalaIT.java
@@ -4,6 +4,7 @@ import api.apiControllers.SalaApiController;
 import api.daos.DaoFactory;
 import api.daos.memory.DaoMemoryFactory;
 import api.dtos.SalaDto;
+import api.dtos.SalaListaDto;
 import http.Client;
 import http.HttpException;
 import http.HttpRequest;
@@ -11,9 +12,9 @@ import http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SalaIT {
@@ -76,5 +77,13 @@ public class SalaIT {
                 .expandPath("invalid-id").body(new SalaDto(null)).put();
         HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
         assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testLeerTodo(){
+        this.testCrearSala();
+        HttpRequest request = HttpRequest.builder(SalaApiController.SALAS).get();
+        List<SalaListaDto> salaLista = (List<SalaListaDto>) new Client().submit(request).getBody();
+        assertTrue(salaLista.size()>=1);
     }
 }
